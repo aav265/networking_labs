413186e647e6055d590e1f0a373b731422798187
aav265 <98988446+aav265@users.noreply.github.com>
2022-02-04 11:56:27 -0500

    Add files via upload

---
+++ skeleton_1.py
@@ -0,0 +1,48 @@
+
+# welcome_assignment_answers
+# Input - All eight questions given in the assignment.
+# Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 " \
+                     "hash generator and use the answer in your code":
+        answer = "42b76fe51778764973077a5a94056724"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a " \
+                     "numeric number":
+        answer = 4
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a " \
+                     "numeric number":
+        answer = 3
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel " \
+                     "posted by a TA?":
+        answer = "mtls"
+    else:
+        answer = "This is not the answer you're looking for."
+    return answer
+# Complete all the questions.
+
+
+if __name__ == "__main__":
+    # use this space to debug and verify that the program works
+    questions = ["Are encoding and encryption the same? - Yes/No", "Is it possible to decrypt a message without a key?"
+                 " - Yes/No", "Is it possible to decode a message without a key? - Yes/No", "Is a hashed message"
+                 " supposed to be un-hashed? - Yes/No", "What is the MD5 hashing value to the following message: 'NYU"
+                 " Computer Networking' - Use MD5 hash generator and use the answer in your code", "Is MD5 a secured"
+                 " hashing algorithm? - Yes/No", "What layer from the TCP/IP model the protocol DHCP belongs to? - The"
+                 " answer should be a numeric number", "What layer of the TCP/IP model the protocol TCP belongs to? -"
+                 " The answer should be a numeric number", "In Slack, what is the secret passphrase posted in the"
+                 " #lab-python-getting-started channel posted by a TA?"]
+    for debug_question in questions:
+        print(debug_question)
+        print(welcome_assignment_answers(debug_question))
+        print('_________________________________________')
